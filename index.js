0f96d69c5c89bdec93f27b2d35135cb4a2cdf619
shubasish <shubasish@recruit.so>
2023-03-02 03:58:34 +0600

    remove comments

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ app.use(bodyParser.json());
 
 app.post("/webhook", (req, res) => {
   // Extract relevant data from incoming message
-  // const message = req.body.messages[0];
-  // const from = message.from;
-  // const text = message.text;
+  const message = req.body.messages[0];
+  const from = message.from;
+  const text = message.text;
 
-  // // Process incoming message
-  // console.log(`Received message from ${from}: ${text}`);
-  // // Send automated response
-  // const response = {
-  //   to: from,
-  //   type: "text",
-  //   text: "Thank you for your message!",
-  // };
+  // Process incoming message
+  console.log(`Received message from ${from}: ${text}`);
+  // Send automated response
+  const response = {
+    to: from,
+    type: "text",
+    text: "Thank you for your message!",
+  };
   // TODO: Send response using the WhatsApp Business API
 
   // Send response back to client
